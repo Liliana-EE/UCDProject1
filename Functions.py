de2b548cf0f80520e158e82b6be019f4747ed576
Lyla <https://github.com/Liliana-EE/UCDProject1>
2021-04-19 16:55:50 +0100

    Defining analysedata functions

--- Functions.py
+++ Functions.py
@@ -9,18 +9,24 @@ def importdata(filename):
     print(data.info)
     return data
 
-importdata("Estimates_of_Disposable_Income_per_Person_2009_2019.csv")
 
 #define cleandata function
 def cleandata(filename):
     # validate how many missing values are in the dataset
     data = pd.read_csv(filename)
-    missing_value=data.isnull().sum
-    print(missing_value)
+    #define missing_values
+    missing_values=data.isnull().sum
+    print(missing_values)
     #fill missing values with 0
     cleaned_data=data.fillna(method="bfill",axis=0).fillna(0)
     print(cleaned_data)
     return cleaned_data
 
-cleandata("Estimates_of_Disposable_Income_per_Person_2009_2019.csv")
+#define joindata function
+def joindata (filename)
+    data = cleandata(filename)
+    #merge X and Y tables on "column name" called altered_data
+    altered_data = data.merge(filename,on="column")
+    #group the results by "column name" then count the number of elements
+
 
