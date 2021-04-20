338f997eda67ae1c5f9eb8baa074747cdff8433b
Lyla <https://github.com/Liliana-EE/UCDProject1>
2021-04-20 04:51:18 +0100

    s - update

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
 # Import pandas as pd
-import pandas as pd
 import numpy as np
-import matplotlib as plt
+import pandas as pd
+
 
 # define importdata function
 def importdata(filename):
@@ -35,16 +35,44 @@ while offset != 0 :
       offset = offset + 1
     print(offset)
 
+#Merge po and stores on subregion and county with fill and
 pop= cleandata("Pop_2016.csv")
 stores = cleandata("Stores.csv")
 
-#Merge pop_2016 and stores tables on subregion
-pop_stores = pd.merge_ordered(pop,stores, on="county", left_on="Total Population",
-                              right_on="stores")
+#pop_stores = pd.merge_ordered(pop,stores,on=["subregion","county"],
+#                              fill_method="ffill")
+#print(pop_stores)
 
-#Add a column named Stores_Opp that
-pop_stores ['Stores_Opp'] = pop_stores['Total Population'] / pop_stores['stores'] - pop_stores['stores']
+#Total_pop = pivot_table(pop, values=["Total population"]),\
+#          rows=["Subregion"],cols= [["Region"], aggfunc] = np.sum, margins=True)
+#Total_pop.stack("Region")
 
-print(pop_stores)
+print(pop.append(pop[["Total Population"]].sum().rename('Gran Total')).fillna(""))
 
+#pop.loc["Gran Total"] = pop[["Total Population"]].sum()
+#pop.loc["Gran Total"] = pop.loc["Total Population"].fillna("")
 
+#for label, _pop in pop.groupby(["Total Population"]):
+#    print(label)
+#   print(_pop)
+#    print()
+
+print(type(pop))
+
+#pop.column.dtypes
+
+import pandas as pd
+#df = pd.DataFrame({"Total Population"})
+
+#fig, ax = plt.subplots()
+#plt.show()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df= importdata("Housing_vs_Comm_Notices.csv")
+
+sns.barplot(x="Year",  y="Value",  hue="Subregion",
+ data=df)
+
+plt.show()
